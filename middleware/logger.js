b5fa0c225ee47a3296943e59cb3268acc967329d
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-05-29 01:07:56 +0530

    colors pkg

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,18 @@
+import colors from "colors";
+
 const logger = (req, res, next) => {
+  const methodColors = {
+    GET: "green",
+    POST: "yellow",
+    PUT: "cyan",
+    DELETE: "red",
+  };
+  const color = methodColors[req.method] || "grey";
+
   console.log(
-    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`[
+      color
+    ]
   );
   next();
 };
