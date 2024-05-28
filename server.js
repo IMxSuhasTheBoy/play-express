838ee8d4290459e5a4e78615294163048143ea2e
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-05-28 21:15:46 +0530

    Add mw to server static files from folder

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const path = require("path");
 const app = express();
 const port = 8000;
 
+//setup static folder (mw)
+app.use(express.static(path.join(__dirname, "public"))); //for static files (css, images, js, etc)
+//no routes require to serve html files by default
+//test url : http://localhost:8000/about.html
+
+/** //for html files
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 app.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "about.html"));
 });
+*/
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
