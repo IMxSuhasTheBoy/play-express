cd906f965714025d53ae096373c42f06be51fba3
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-05-29 00:38:53 +0530

    middleware

--- serverES6.js
+++ serverES6.js
@@ -1,12 +1,16 @@
 import express from "express";
 
 import posts from "./routes/posts.js";
+import logger from "./middleware/logger.js";
 const app = express();
 const PORT = process.env.PORT || 8001;
 
 //body parser mw
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// mw
+app.use(logger);
 
 //Routes import & declaration  as smw
 app.use("/api/v1/posts", posts);
