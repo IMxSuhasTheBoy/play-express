0d46fc4a1323288570a20bda3ca9976a68c15721
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-05-28 21:55:27 +0530

    req.query()

--- serverJSON.js
+++ serverJSON.js
@@ -20,7 +20,17 @@ let posts = [
 
 //get all posts
 app.get("/api/v1/posts", (req, res) => {
-  res.json(posts);
+  //! accept only the data that can be accepted(expected data)
+  console.log(req.query);
+  //test : http://localhost:8000/api/v1/posts?limit=2&sort=desc
+
+  const limit = parseInt(req.query.limit) || 3;
+
+  if (!isNaN(limit) && limit > 0) {
+    res.json(posts.slice(0, limit));
+  } else {
+    res.json(posts);
+  }
 });
 
 //get single post
